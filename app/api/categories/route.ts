e6a250b66996af23961df24e1bd4728b7d1fa62c
mxrain <34500180+mxrain@users.noreply.github.com>
2024-10-17 09:03:58 +0000

    add

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,17 +1,19 @@
 import { NextResponse } from 'next/server';
-  
+import axios from 'axios';
+
 const DATA_URL = `https://raw.gitmirror.com/${process.env.NEXT_PUBLIC_GITHUB_OWNER}/${process.env.NEXT_PUBLIC_GITHUB_REPO}/master/src/db/db.json`;
 
 export async function GET() {
+  const headers = new Headers();
+  headers.set('Cache-Control', 'no-store, max-age=0');
   try {
-    const response = await fetch(DATA_URL);
-    if (!response.ok) {
-      throw new Error('获取数据失败');
-    }
-    const data = await response.json();
-    return NextResponse.json(data);
+    const response = await axios.get(DATA_URL);
+    const data = response.data;
+    return NextResponse.json(data, { headers });
+
+
   } catch (error) {
-    console.error('获取类别数据时出错:', error);
-    return NextResponse.json({ error: '获取类别数据失败' }, { status: 500 });
+    console.error('获取分类数据时出错:', error);
+    return NextResponse.json({ error: '获取分类数失败' }, { status: 500 });
   }
 }
